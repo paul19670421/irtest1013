5e9adb9951ab18c32e5bf3ebe7bc58e752da3a4d
paul19670421 <38580511+paul19670421@users.noreply.github.com>
2019-10-13 15:07:49 +0800

    Add files via upload

--- maqueen.ts
+++ maqueen.ts
@@ -27,7 +27,62 @@ namespace maqueen {
 
     export enum aMotors {
         
+//% blockId="M1" block="M1"
+        M1 = 0,
+        
+//% blockId="M2" block="M2"
+        M2 = 1,
+        
+//% blockId="All" block="All"
+        All = 2
+    }
 
+    export enum aServos {
+        
+//% blockId="S1" block="S1"
+        S1 = 0,
+        
+//% blockId="S2" block="S2"
+        S2 = 1
+    }
+
+    export enum Dir {
+        
+//% blockId="CW" block="CW"
+        CW = 0x0,
+        
+//% blockId="CCW" block="CCW"
+        CCW = 0x1
+    }
+
+    export enum Patrol {
+       
+ //% blockId="PatrolLeft" block="PatrolLeft"
+        PatrolLeft = 13,
+        
+//% blockId="PatrolRight" block="PatrolRight"
+        PatrolRight = 14
+    }
+
+    export enum LED {
+        
+//% blockId="LEDLeft" block="LEDLeft"
+        LEDLeft = 8,
+        
+//% blockId="LEDRight" block="LEDRight"
+        LEDRight = 12
+    }
+
+    export enum LEDswitch {
+        
+//% blockId="turnOn" block="turnOn"
+        turnOn = 0x01,
+        
+//% blockId="turnOff" block="turnOff"
+        turnOff = 0x00
+    }
+
+    
 //% advanced=true shim=maqueenIR::initIR
     function initIR(pin: Pins): void {
         return
@@ -135,12 +190,72 @@ namespace maqueen {
     //% index.fieldEditor="gridpicker" index.fieldOptions.columns=2
     
 //% direction.fieldEditor="gridpicker" direction.fieldOptions.columns=2
-          
-//% blockId=motor_motorStopAll block="Motor Stop All"
-       
+    export function MotorRun(index: aMotors, direction: Dir, speed: number): void {
+        let buf = pins.createBuffer(3);
+        if (index == 0) {
+            buf[0] = 0x00;
+            buf[1] = direction;
+            buf[2] = speed;
+            pins.i2cWriteBuffer(0x10, buf);
+        }
+        if (index == 1) {
+            buf[0] = 0x02;
+            buf[1] = direction;
+            buf[2] = speed;
+            pins.i2cWriteBuffer(0x10, buf);
+        }
+        if (index == 2) {
+            buf[0] = 0x00;
+            buf[1] = direction;
+            buf[2] = speed;
+            pins.i2cWriteBuffer(0x10, buf);
+            buf[0] = 0x02;
+            pins.i2cWriteBuffer(0x10, buf);
+        }
+    }
 
+    //% weight=20
     
-//% blockId=read_Patrol block="Read Patrol|%patrol"
+//% blockId=motor_motorStop block="Motor stop|%motors"
+    
+//% motors.fieldEditor="gridpicker" motors.fieldOptions.columns=2 
+    export function motorStop(motors: aMotors): void {
+        let buf = pins.createBuffer(3);
+        if (motors == 0) {
+            buf[0] = 0x00;
+            buf[1] = 0;
+            buf[2] = 0;
+            pins.i2cWriteBuffer(0x10, buf);
+        }
+        if (motors == 1) {
+            buf[0] = 0x02;
+            buf[1] = 0;
+            buf[2] = 0;
+            pins.i2cWriteBuffer(0x10, buf);
+        }
+
+        if (motors == 2) {
+            buf[0] = 0x00;
+            buf[1] = 0;
+            buf[2] = 0;
+            pins.i2cWriteBuffer(0x10, buf);
+            buf[0] = 0x02;
+            pins.i2cWriteBuffer(0x10, buf);
+        }
+
+    }
+    /*
+        
+//% weight=10
+        
+//% blockId=motor_motorStopAll block="Motor Stop All"
+        export function motorStopAll(): void {
+            let buf = pins.createBuffer(3);
+            
+        }
+    */
+    //% weight=20
+    //% blockId=read_Patrol block="Read Patrol|%patrol"
     //% patrol.fieldEditor="gridpicker" patrol.fieldOptions.columns=2 
     export function readPatrol(patrol: Patrol): number {
         if (patrol == Patrol.PatrolLeft) {
